1791a8a707e9814fabeeea7c3a903bf7b07767a7
Jared Slosberg <jslosbe1@jhmi.edu>
2021-10-04 16:28:04 -0400

    reprocess UMAP with read depth regressed out. update 6mo and mens scripts

--- run_all_analysis.sh
+++ run_all_analysis.sh
@@ -7,24 +7,24 @@ mkdir -p data/ plots/
 #Requires building kallistobus.yml
 bash ./preprocessing/scripts/run_all.sh
 
-R -e "rmarkdown::render('scripts/MES_6mo_LMMP_analysis.Rmd')"
+R -e "rmarkdown::render('scripts/MES_6mo_LMMP_analysis.Rmd', output_format = 'html_document')"
 
 #depends on MES_6mo_LMMP_analysis.Rmd
-R -e "rmarkdown::render('scripts/tricycle_analysis.Rmd')"
+R -e "rmarkdown::render('scripts/tricycle_analysis.Rmd', output_format = 'html_document')"
 
 #depends on MES_6mo_LMMP_analysis.Rmd
-R -e "rmarkdown::render('scripts/pattern_analysis.Rmd')"
+R -e "rmarkdown::render('scripts/pattern_analysis.Rmd', output_format = 'html_document')"
 
 #depends on pattern_analysis.Rmd
-R -e "rmarkdown::render('scripts/NMF_UMI_correlation.Rmd')"
+R -e "rmarkdown::render('scripts/NMF_UMI_correlation.Rmd', output_format = 'html_document')"
 
 #depends on MES_6mo_LMMP_analysis.Rmd
-R -e "rmarkdown::render('scripts/6mo_MENS_followup.Rmd')"
+R -e "rmarkdown::render('scripts/6mo_MENS_followup.Rmd', output_format = 'html_document')"
 
 #Velocity analysis
 
 #depends on MES_6mo_LMMP_analysis.Rmd
-R -e "rmarkdown::render('scripts/6mo_MENS_followup.Rmd')"
+R -e "rmarkdown::render('scripts/6mo_MENS_followup.Rmd', output_format = 'html_document')"
 
 #Requires building scvelocity.yml
 conda activate scVelocity
